1b59faa68fd4d393a28865fab65bf218eb0f8fbc
Neal Vos <nealvos1353@gmail.com>
2018-09-28 10:15:30 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+		<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Index.php</title>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-112418499-4"></script>
+	<script>
+  		window.dataLayer = window.dataLayer || [];
+  		function gtag(){dataLayer.push(arguments);}
+  		gtag('js', new Date());
+
+  		gtag('config', 'UA-112418499-4');
+	</script>
+</head>
+
+<body>
+	<main>
+		<ul>
+			<li><a href="javascript:void(0)"><i class="fa fa-home">fa fa-home</i>&emsp;Home</a></li>
+			<li><a href="javascript:void(0)"><i class="fa fa-puzzle-piece">fa fa-puzzle-piece</i>&emsp;Memory</a></li>
+			<li><a href="javascript:void(0)"><i class="fa fa-sort-numeric-up">fa fa-sort-numeric-up</i>&emsp;Getallenreeks</a></li>
+			<li><a href="javascript:void(0)"><i class="fa fa-images">fa fa-images</i>&emsp;Picture Puzzle</a></li>
+		</ul>
+	</main>
+</body>
+</html>
